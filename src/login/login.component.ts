bd288fd84e8cbaa0fa6fa3eab9828e3073e648c9
ashish-kul <akashish.ak@gmail.com>
2026-01-30 19:24:05 +0530

    preventing double submission

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { APIServices } from '../services/api.services';
 import { NotificationComponent } from "../notification/notification.component";
 import { Router } from '@angular/router';
+import { exhaustMap, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -17,6 +18,7 @@ export class LoginComponent implements OnInit {
   notificationType = '';
   notificationMessage = '';
   notificationVisibility = false;
+  loginClick$ = new Subject<any>();
 
   constructor(
     private service: APIServices,
@@ -30,6 +32,16 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     //Clear localStorage when user landing on login page
     localStorage.clear();
+
+    this.loginClick$.pipe(
+      exhaustMap((payload) => this.service.login(payload))
+    ).subscribe((res: any) => {
+      this.signInForm.reset();
+      localStorage.setItem("token", res?.accessToken)
+      this.router.navigate(['/dashboard']);
+    }, (error: any) => {
+      this.setNotificationDetails(JSON.stringify(error?.error), 'error', true);
+    });
   }
 
   signIn() {
@@ -37,18 +49,13 @@ export class LoginComponent implements OnInit {
       //To display error message on click of the button
       this.signInForm.markAllAsTouched();
     } else {
+
       //Valid form
-      const payload = {
+      const payload: any = {
         "email": this.signInForm.controls['email']?.value,
         "password": this.signInForm.controls['password']?.value
       };
-      this.service.login(payload).subscribe((res: any) => {
-        this.signInForm.reset();
-        localStorage.setItem("token", res?.accessToken)
-        this.router.navigate(['/dashboard']);
-      }, (error: any) => {
-        this.setNotificationDetails(JSON.stringify(error?.error), 'error', true);
-      });
+      this.loginClick$.next(payload);
     }
   }
 
